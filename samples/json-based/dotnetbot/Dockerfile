34e4c85c5581c2f6714e281c6efbad0eb198a76f
Jeff <jeffrey.pierson@gmail.com>
2016-12-27 01:31:20 -0500

    Separate csproj and project.json samples

---
+++ samples/json-based/dotnetbot/Dockerfile
@@ -0,0 +1,4 @@
+FROM microsoft/dotnet
+COPY bin/Debug/netcoreapp1.0 app
+WORKDIR app
+ENTRYPOINT ["dotnet", "dotnetbot.dll"]
---
+++ samples/json-based/helloworld/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorldSample 
+{
+	public static class Program 
+	{
+		public static void Main() 
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
