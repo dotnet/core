046923a93e68946b8a1f8054eae62ecb245efef5
Rich Lander <rlander@microsoft.com>
2017-07-03 15:05:59 -0700

    Add sample with .NET Framework dependency

---
+++ samples/core-with-framework-dependency/CoreWithFrameworkDependency/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+using ClassLibrary1;
+
+namespace CoreWithFrameworkDependency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World from .NET Core!");
+            WriteLine(Class1.GetMessage());
+        }
+    }
+}
