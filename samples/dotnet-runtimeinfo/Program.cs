ce0a0ef5bb3b77023ce3509fbc08aea729c208d4
Jiri Cincura ↹ <jiri@cincura.net>
2020-11-11 23:06:28 +0100

    Simpler Linux check. (#5106)

--- samples/dotnet-runtimeinfo/Program.cs
+++ samples/dotnet-runtimeinfo/Program.cs
@@ -26,7 +26,7 @@ namespace RuntimeEnvironment
             WriteLine($"{nameof(Environment.ProcessorCount)}: {Environment.ProcessorCount}");
             WriteLine();
 
-            if(RuntimeInformation.OSDescription.StartsWith("Linux") && Directory.Exists("/sys/fs/cgroup"))
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && Directory.Exists("/sys/fs/cgroup"))
             {
                 WriteLine("**CGroup info**");
                 WriteLine($"cfs_quota_us: {ReadAllLines("/sys/fs/cgroup/cpu/cpu.cfs_quota_us")[0]}");
