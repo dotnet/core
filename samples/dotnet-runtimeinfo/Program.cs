f0e243974632ba779f05781cdc6aadef728c84b2
Rich Lander <rlander@microsoft.com>
2020-11-11 16:45:19 -0800

    Update to top-level projects

--- samples/dotnet-runtimeinfo/Program.cs
+++ samples/dotnet-runtimeinfo/Program.cs
@@ -5,34 +5,26 @@ using System.Runtime.InteropServices;
 using static System.Console;
 using static System.IO.File;
 
-namespace RuntimeEnvironment
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            WriteLine("**.NET information");
-            WriteLine($"{nameof(Environment.Version)}: {Environment.Version}");
-            WriteLine($"{nameof(RuntimeInformation.FrameworkDescription)}: {RuntimeInformation.FrameworkDescription}");
-            var assemblyInformation = ((AssemblyInformationalVersionAttribute[])typeof(object).Assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false))[0];
-            var informationalVersionSplit = assemblyInformation.InformationalVersion.Split('+');
-            WriteLine($"Libraries version: {informationalVersionSplit[0]}");
-            WriteLine($"Libraries hash: {informationalVersionSplit[1]}");
-            WriteLine();
-            WriteLine("**Environment information");
-            WriteLine($"{nameof(RuntimeInformation.OSDescription)}: {RuntimeInformation.OSDescription}");
-            WriteLine($"{nameof(Environment.OSVersion)}: {Environment.OSVersion}");
-            WriteLine($"{nameof(RuntimeInformation.OSArchitecture)}: {RuntimeInformation.OSArchitecture}");
-            WriteLine($"{nameof(Environment.ProcessorCount)}: {Environment.ProcessorCount}");
-            WriteLine();
+var assemblyInformation = ((AssemblyInformationalVersionAttribute[])typeof(object).Assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false))[0];
+var informationalVersionSplit = assemblyInformation.InformationalVersion.Split('+');
 
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && Directory.Exists("/sys/fs/cgroup"))
-            {
-                WriteLine("**CGroup info**");
-                WriteLine($"cfs_quota_us: {ReadAllLines("/sys/fs/cgroup/cpu/cpu.cfs_quota_us")[0]}");
-                WriteLine($"memory.limit_in_bytes: {ReadAllLines("/sys/fs/cgroup/memory/memory.limit_in_bytes")[0]}");
-                WriteLine($"memory.usage_in_bytes: {ReadAllLines("/sys/fs/cgroup/memory/memory.usage_in_bytes")[0]}");
-            }
-        }
-    }
+WriteLine("**.NET information");
+WriteLine($"{nameof(Environment.Version)}: {Environment.Version}");
+WriteLine($"{nameof(RuntimeInformation.FrameworkDescription)}: {RuntimeInformation.FrameworkDescription}");
+WriteLine($"Libraries version: {informationalVersionSplit[0]}");
+WriteLine($"Libraries hash: {informationalVersionSplit[1]}");
+WriteLine();
+WriteLine("**Environment information");
+WriteLine($"{nameof(RuntimeInformation.OSDescription)}: {RuntimeInformation.OSDescription}");
+WriteLine($"{nameof(Environment.OSVersion)}: {Environment.OSVersion}");
+WriteLine($"{nameof(RuntimeInformation.OSArchitecture)}: {RuntimeInformation.OSArchitecture}");
+WriteLine($"{nameof(Environment.ProcessorCount)}: {Environment.ProcessorCount}");
+WriteLine();
+
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && Directory.Exists("/sys/fs/cgroup"))
+{
+    WriteLine("**CGroup info");
+    WriteLine($"cfs_quota_us: {ReadAllLines("/sys/fs/cgroup/cpu/cpu.cfs_quota_us")[0]}");
+    WriteLine($"memory.limit_in_bytes: {ReadAllLines("/sys/fs/cgroup/memory/memory.limit_in_bytes")[0]}");
+    WriteLine($"memory.usage_in_bytes: {ReadAllLines("/sys/fs/cgroup/memory/memory.usage_in_bytes")[0]}");
 }
