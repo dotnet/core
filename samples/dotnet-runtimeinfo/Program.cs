4e4e47d0cafec816a5f1f707e3cf8469dd9eaa9a
Dan Moseley <danmose@microsoft.com>
2023-09-14 16:17:10 -0600

    tune logo

--- samples/dotnet-runtimeinfo/Program.cs
+++ samples/dotnet-runtimeinfo/Program.cs
@@ -9,34 +9,38 @@ string nl = Environment.NewLine;
 
 // Variant of https://github.com/dotnet/core/tree/main/samples/dotnet-runtimeinfo
 // Ascii text: https://ascii.co.uk/text (Univers font)
-WriteLine(
-$"         42{nl}" +
-$"         42              ,d                             ,d{nl}" +
-$"         42              42                             42{nl}" +
-$" ,adPPYb,42  ,adPPYba, MM42MMM 8b,dPPYba,   ,adPPYba, MM42MMM{nl}" +
-$"a8\"    `Y42 a8\"     \"8a  42    42P\'   `\"8a a8P_____42   42{nl}" +
-$"8b       42 8b       d8  42    42       42 8PP\"\"\"\"\"\"\"   42{nl}" +
-$"\"8a,   ,d42 \"8a,   ,a8\"  42,   42       42 \"8b,   ,aa   42,{nl}" +
-$" `\"8bbdP\"Y8  `\"YbbdP\"\'   \"Y428 42       42  `\"Ybbd8\"\'   \"Y428{nl}");
+WriteLine(""""""""
+         88
+         88              ,d                             ,d
+         88              88                             88
+ ,adPPYb,88  ,adPPYba, MM88MMM 8b,dPPYba,   ,adPPYba, MM88MMM
+a8"    `Y88 a8"     "8a  88    88P'   `"8a a8P_____88   88
+8b       88 8b       d8  88    88       88 8PP"""""""   88
+"8a,   ,d88 "8a,   ,a8"  88,   88       88 "8b,   ,aa   88,
+ `"8bbdP"Y8  `"YbbdP"'   "Y888 88       88  `"Ybbd8"'   "Y888
+
+"""""""");
 
 AssemblyInformationalVersionAttribute assemblyInformation = ((AssemblyInformationalVersionAttribute[])typeof(object).Assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false))[0];
 string[] informationalVersionSplit = assemblyInformation.InformationalVersion.Split('+');
 
-WriteLine("**.NET information");
-WriteLine($"{nameof(Environment.Version)}: {Environment.Version}");
-WriteLine($"{nameof(RuntimeInformation.FrameworkDescription)}: {RuntimeInformation.FrameworkDescription}");
-WriteLine($"Libraries version: {informationalVersionSplit[0]}");
-WriteLine($"Libraries hash: {informationalVersionSplit[1]}");
+WriteLine("=== .NET information ===");
+WriteLine("{0,-20} {1}", "Environment.Version", Environment.Version);
+WriteLine("{0,-20} {1}", "FrameworkDescription", RuntimeInformation.FrameworkDescription);
+WriteLine("{0,-20} {1}", "Libraries version", informationalVersionSplit[0]);
+WriteLine("{0,-20} {1}", "Libraries hash", informationalVersionSplit[1]);
+WriteLine("{0,-20} {1}", "Runtime identifier", RuntimeInformation.RuntimeIdentifier);
+WriteLine("{0,-20} {1}", "Runtime location", Path.GetDirectoryName(typeof(object).Assembly.Location));
 WriteLine();
-WriteLine("**Environment information");
-WriteLine($"{nameof(Environment.ProcessorCount)}: {Environment.ProcessorCount}");
-WriteLine($"{nameof(RuntimeInformation.OSArchitecture)}: {RuntimeInformation.OSArchitecture}");
-WriteLine($"{nameof(RuntimeInformation.OSDescription)}: {RuntimeInformation.OSDescription}");
-WriteLine($"{nameof(Environment.OSVersion)}: {Environment.OSVersion}");
+WriteLine("===Environment information ===");
+WriteLine($"{nameof(Environment.ProcessorCount),-20} {Environment.ProcessorCount}");
+WriteLine($"{nameof(RuntimeInformation.OSArchitecture),-20} {RuntimeInformation.OSArchitecture}");
+WriteLine($"{nameof(RuntimeInformation.OSDescription),-20} {RuntimeInformation.OSDescription}");
+WriteLine($"{nameof(Environment.OSVersion),-20} {Environment.OSVersion}");
 
 // Linux Pretty Name
 const string OSRel = "/etc/os-release";
-if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && 
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) &&
     File.Exists(OSRel))
 {
   const string PrettyName = "PRETTY_NAME";
